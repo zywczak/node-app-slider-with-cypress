d82231316fae73c2dac2dca4187ababd822bd002
Adrian Widlak <awidlak@adrianwii.pl>
2024-03-21 09:24:05 +0100

    Setup sample express app and cypress e2e tests

---
+++ cypress/e2e/slider.cy.js
@@ -0,0 +1,18 @@
+describe('Swiper Gallery Test', function () {
+  it('Checks if second slide contains "United Kingdom"', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+  });
+});
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if third slide contains "Paris"', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Paris');
+  });
+});

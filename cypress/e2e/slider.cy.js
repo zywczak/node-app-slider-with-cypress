32b980345cd698390a389bd0a282d480d491a9a4
zywczak <pjzywczak@gmail.com>
2025-03-06 18:25:42 +0100

    Update slider.cy.js

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -17,50 +17,24 @@ describe('Swiper Gallery Test', function () {
   });
 });
 
-describe('Swiper Gallery Test', function () {
-  it('Checks if slides are swiped correctly', function () {
-    cy.visit('http://localhost:3000');
-    cy.get('.swiper-slide-active').should('contain', 'Rome');
-    cy.get('.swiper-slide-active').should('contain', 'Italy');
-    cy.get('.swiper-button-next').click();
-    cy.wait(2000);
-    cy.get('.swiper-slide-active').should('contain', 'London');
-    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
-    cy.get('.swiper-button-next').click();
-    cy.wait(2000);
-    cy.get('.swiper-slide-active').should('contain', 'Paris');
-    cy.get('.swiper-slide-active').should('contain', 'France');
-  });
-});
-
-
-
 describe('Swiper Gallery Tests', function () {
   beforeEach(() => {
     cy.visit('http://localhost:3000');
   });
 
-  it('Checks if second slide contains "United Kingdom"', function () {
-    cy.get('.swiper-button-next').click();
-    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
-  });
-
-  it('Checks if third slide contains "Paris"', function () {
-    cy.get('.swiper-button-next').click();
-    cy.wait(1000);
-    cy.get('.swiper-button-next').click();
-    cy.get('.swiper-slide-active').should('contain', 'Paris');
-  });
-
   it('Ensures navigation buttons work properly', function () {
-    cy.get('.swiper-button-next').click();
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+
+    cy.get('.swiper-button-next').should('be.visible').click();
+    cy.wait(500);
     cy.get('.swiper-slide-active').should('contain', 'London');
-    cy.get('.swiper-button-prev').click();
-    cy.wait(1000);
+
+    cy.get('.swiper-button-prev').should('be.visible').click();
+    cy.wait(500); 
     cy.get('.swiper-slide-active').should('contain', 'Rome');
   });
 
-  it('Verifies each slide has a title and description', function () {
+  it('Verifies each slide has a right title and description', function () {
     const slides = [
       { title: 'Rome', description: 'Italy' },
       { title: 'London', description: 'United Kingdom' },
@@ -79,9 +53,9 @@ describe('Swiper Gallery Tests', function () {
 
   it('Checks if gallery adapts to different screen sizes', function () {
     const viewports = [
-      [1920, 1080], // Desktop
-      [1024, 768],  // Tablet
-      [375, 667]    // Mobile
+      [1920, 1080],
+      [1024, 768],
+      [375, 667]
     ];
 
     viewports.forEach((size) => {
